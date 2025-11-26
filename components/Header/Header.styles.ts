e1571be47e727a860b108465f4de46dbf37a3c5a
micaelenn <mirandamicaelen@gmail.com>
2025-11-26 11:23:12 -0300

    fix: adjustment on header height

--- components/Header/Header.styles.ts
+++ components/Header/Header.styles.ts
@@ -7,9 +7,9 @@ export const Component = styled.header`
   align-items: center;
   justify-content: space-between;
   flex-direction: column;
+  height: 75px;
   gap: 10px;
   margin-bottom: 40px;
-  padding: 25px;
 
   @media(min-width: ${medias.landscape}) {
     flex-direction: row;
